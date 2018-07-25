69ec68b2c284e555b64923ff16317710464a10ed
Kalvin Suting <sutingk@wwu.edu>
2018-07-25 10:50:23 -0700

    added feature A

--- git.c
+++ git.c
@@ -4,3 +4,11 @@ int main(int argc, char const *argv[]) {
   printf("this is a test, does this work?\n");
   return 0;
 }
+
+int feature_A(int n){
+  if(n == 0){
+    return 1;
+  } else {
+    return 0;
+  }
+}
